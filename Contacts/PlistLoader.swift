c378c539e06f64a8b4e7da545a315947ede72404
Francisco Ozuna <ozuna252@hotmail.com>
2020-03-18 21:58:24 -0400

    Added sample data to work with and helper code to bring that data into our data model.

---
+++ Contacts/PlistLoader.swift
@@ -0,0 +1,36 @@
+//
+//  PlistLoader.swift
+//  Contacts
+//
+//  Created by Francisco Ozuna on 3/18/20.
+//  Copyright © 2020 Francisco Ozuna. All rights reserved.
+//
+
+import Foundation
+
+enum PlistError: Error {
+    case invalidResource
+    case parsingFailure
+}
+
+class PlistLoader {
+    static func array(fromFile name: String, ofType type: String) throws -> [[String: String]] {
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else {
+            throw PlistError.invalidResource
+        }
+        
+        guard let array = NSArray(contentsOfFile: path) as? [[String: String]] else {
+            throw PlistError.parsingFailure
+        }
+        
+        return array
+    }
+}
+
+class ContactsSource {
+    static var contacts: [Contact] {
+        let data = try! PlistLoader.array(fromFile: "ContactsDB", ofType: "plist")
+        return data.flatMap { Contact(dictionary: $0) }
+    }
+    
+}
