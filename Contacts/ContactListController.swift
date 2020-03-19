f209b151791b82cb4d19e8cf68d031061c508b7f
Francisco Ozuna <ozuna252@hotmail.com>
2020-03-19 16:49:04 -0400

    Created and customized table view cell to display contacts. Set cell textLabel, imageView and detailTextLabel property to each contact's firstName, image and lastName respectively using the Basic cell style.

--- Contacts/ContactListController.swift
+++ Contacts/ContactListController.swift
@@ -24,5 +24,17 @@ class ContactListController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contacts.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+        
+        // since all rows are going to be contained in a single section we can just discard the section information and use the row value to get a contact from the contacts array. This way, when we're setting up row zero, IndexPath.row is zero.
+        let contact = contacts[indexPath.row]
+        // the properties below are optional because in some TableViewCell style's the image does not exist in others it does. Because its an optional property, when this is nil it should just silently and gracefully fail without creating any errors.
+        cell.textLabel?.text = contact.firstName
+        cell.imageView?.image = contact.image
+        cell.detailTextLabel?.text = contact.lastName
+        return cell
+    }
 
 }
