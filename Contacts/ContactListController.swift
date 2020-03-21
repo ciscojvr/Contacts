fd1353e951e0b7afd73ccdb843fb0f76773106b1
Francisco Ozuna <ozuna252@hotmail.com>
2020-03-21 14:06:28 -0400

    Added code to display sectioned data and updated the implementations to the data source methods.

--- Contacts/ContactListController.swift
+++ Contacts/ContactListController.swift
@@ -31,26 +31,38 @@ extension ContactsSource {
 
 class ContactListController: UITableViewController {
     
-    var contacts = ContactsSource.contacts
+    var sections = ContactsSource.sectionedContacts
+    let sectionTitles = ContactsSource.sortedUniqueFirstLetters
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     // MARK: - Data Source
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sectionTitles
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sections.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
+        // when the table view is setting itself up and calls this method, it always provides an argument for which sections its currently in
+        return sections[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
         
         // since all rows are going to be contained in a single section we can just discard the section information and use the row value to get a contact from the contacts array. This way, when we're setting up row zero, IndexPath.row is zero.
-        let contact = contacts[indexPath.row]
+//        let contact = contacts[indexPath.row]
+        let contact = sections[indexPath.section][indexPath.row]
+        
         // the properties below are optional because in some TableViewCell style's the image does not exist in others it does. Because its an optional property, when this is nil it should just silently and gracefully fail without creating any errors.
         cell.textLabel?.text = contact.firstName
         cell.imageView?.image = contact.image
@@ -62,7 +74,7 @@ class ContactListController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showContact" {
             if let indexPath = tableView.indexPathForSelectedRow { // this lets us know which row was tapped on. this is an optional property (tableView.indexPathForSelectedRow) because if no row is selected, the value is obviously nil. So we unwrap using if let statement.
-                let contact = contacts[indexPath.row] // With an index path, since the data source, the contacts array, and rows correspond to one another we use this to figure out which contact the user selected.
+                let contact = sections[indexPath.section][indexPath.row] // With an index path, since the data source, the contacts array, and rows correspond to one another we use this to figure out which contact the user selected.
                 
                 // gets the destination of the segue. Need to go to navigation controller first because that's where ContactDetailController is embedded in.
                 guard let navigationController = segue.destination as? UINavigationController,
